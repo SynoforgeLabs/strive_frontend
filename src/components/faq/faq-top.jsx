ae31b1d701fd1d529272becd7c444fac1842dd67
blvkct <blvkct@gmail.com>
2025-04-09 05:33:37 +0530

    faq page done and small fix in reviews page

--- src/components/faq/faq-top.jsx
+++ src/components/faq/faq-top.jsx
@@ -1,42 +1,43 @@
-import '../../css/faq/faq-top.css'
-
+import "../../css/faq/faq-top.css";
 
 function Faqtop() {
     return (
-
-        <section className="row container-fluid section d-flex g-0 z-index-0 faq-top-main" id='faq-top'>
+        <section className="row container-fluid section d-flex g-0 z-index-0 faq-top-main" id="faq-top">
             <div className="row col-9 d-flex faq-top-sec">
-                <h1 className='fw-bold faq-top-main-h'>Frequently Asked Questions</h1>
-                    <div class="row accordion d-flex gap-3 faq-top-ac-main" id="accordionExample">
-                        <div class="accordion-item col-4">
+                <h1 className="fw-bold faq-top-main-h">Frequently Asked Questions</h1>
+                <div className="row d-flex gap-3 faq-top-q-main">
+                    <div class="accordion col-5" id="faq-top-q-1">
+                        <div class="accordion-item" id="faq-top-ac-item">
                             <h2 class="accordion-header">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                Accordion Item #1
+                            <button class="btn primary-button fw-bold w-100 faq-top-q" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                            Payment Policy
                             </button>
                             </h2>
-                            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                            </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item col-4">
-                            <h2 class="accordion-header">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                Accordion Item #1
-                            </button>
-                            </h2>
-                            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
                             <div class="accordion-body">
                                 <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                             </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                    <div class="accordion col-5" id="faq-top-q-2">
+                        <div class="accordion-item" id="faq-top-ac-item">
+                            <h2 class="accordion-header">
+                            <button class="btn primary-button fw-bold w-100 faq-top-q" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo" id="faq-top-q-2">
+                            Cancellation Policy
+                            </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse show">
+                            <div class="accordion-body">
+                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>               
+            </div>
         </section>
-
     );
 }
 
-export default Faqtop
+export default Faqtop;
---
+++ src/components/faq/faq-ucat.jsx
@@ -0,0 +1,143 @@
+import "../../css/faq/faq-ucat.css";
+
+function Ucat() {
+
+    return (
+        <section
+            className="row container-fluid section d-flex g-0 z-index-0 faq-ucat-main"
+            id="faq-ucat-main"
+        >
+            <div className="row col-9 d-flex faq-ucat-sec">
+                <h1 className="fw-bold d-flex faq-ucat-main-h">
+                UCAT
+                </h1>
+
+                <div class="accordion d-flex gap-3" id="faq-ucat-accordians-main">
+                    <div class="accordion-item" id="faq-ucat-ac-item">
+                        <h2 class="accordion-header">
+                            <button
+                                class="btn primary-button d-flex fw-bold w-100 faq-ucat-q"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#panelsStayOpen-collapseucat1"
+                                aria-expanded="true"
+                                aria-controls="panelsStayOpen-collapseucat1"
+                            >
+                                How can I get started?
+                            </button>
+                        </h2>
+                        <div
+                            id="panelsStayOpen-collapseucat1"
+                            class="accordion-collapse collapse show"
+                        >
+                            <div class="accordion-body">
+                            You can get started by filling this form. 
+Once we establish a group of at least 4, and no more than 8 students, we will get the free academic skills consultation organised!
+
+Once the free academic skills consultation is complete, you will start having weekly sessions with your group and expert tutor! 
+
+Please note: the free academic skills consultation will not be offered if you are joining an existing group. 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item" id="faq-ucat-ac-item">
+                        <h2 class="accordion-header">
+                            <button
+                                class="btn primary-button d-flex fw-bold w-100 faq-ucat-q"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#panelsStayOpen-collapseucat2"
+                                aria-expanded="false"
+                                aria-controls="panelsStayOpen-collapseucat2"
+                            >
+                                What will I need for sessions?
+                            </button>
+                        </h2>
+                        <div
+                            id="panelsStayOpen-collapseucat2"
+                            class="accordion-collapse collapse"
+                        >
+                            <div class="accordion-body">
+                            You will need access to the Medify questionbank - the gold standard for UCAT questions. While we have no affiliation with Medify, we know that their questionbank closely represents the standard of questions on the UCAT exam. 
+You can purchase Medify using the following link: https://www.medify.com.au/ucat
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item" id="faq-ucat-ac-item">
+                        <h2 class="accordion-header">
+                            <button
+                                class="btn primary-button d-flex fw-bold w-100 faq-ucat-q"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#panelsStayOpen-collapseucat3"
+                                aria-expanded="false"
+                                aria-controls="panelsStayOpen-collapseucat3"
+                            >
+                                How do payments work?
+                            </button>
+                        </h2>
+                        <div
+                            id="panelsStayOpen-collapseucat3"
+                            class="accordion-collapse collapse"
+                        >
+                            <div class="accordion-body">
+                            You will never be charged before a session is completed. For the first lesson with your tutor, you will receive an invoice. Please ensure that this invoice is paid within 48 hours.
+You can make payment via credit/debit card, Google/Apple Pay or direct debit. We do not accept other payment methods.
+For sessions thereafter, your card will be charged automatically after each lesson. Stripe’s secure payment system handles all payments. Once your fees have been received, the tutor is paid immediately.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item" id="faq-ucat-ac-item">
+                        <h2 class="accordion-header">
+                            <button
+                                class="btn primary-button d-flex fw-bold w-100 faq-ucat-q"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#panelsStayOpen-collapseucat4"
+                                aria-expanded="false"
+                                aria-controls="panelsStayOpen-collapseucat4"
+                            >
+                                I would prefer private sessions. How can I organise them?
+                            </button>
+                        </h2>
+                        <div
+                            id="panelsStayOpen-collapseucat4"
+                            class="accordion-collapse collapse"
+                        >
+                            <div class="accordion-body">
+                            We understand that some students work best when they have 1-on-1 tutor support. 
+You can get started with private sessions for UCAT preparation by finding a tutor - please filter by subject - UCAT and select a suitable tutor. 
+Once you contact the tutor you select, they will then schedule a free academic skills consultation and once you complete this, you’re all set to start sessions!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item" id="faq-ucat-ac-item">
+                        <h2 class="accordion-header">
+                            <button
+                                class="btn primary-button d-flex fw-bold w-100 faq-ucat-q"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#panelsStayOpen-collapseucat5"
+                                aria-expanded="false"
+                                aria-controls="panelsStayOpen-collapseucat5"
+                            >
+                                Do you have any policies, terms and conditions I need to be aware of?
+                            </button>
+                        </h2>
+                        <div
+                            id="panelsStayOpen-collapseucat5"
+                            class="accordion-collapse collapse"
+                        >
+                            <div class="accordion-body">
+                            There are several important policies outlined in our Terms and Conditions . As a Student or Tutor at Strive Academics, you agree to all terms set out in our Terms and Conditions.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section >
+    );
+}
+
+export default Ucat;
