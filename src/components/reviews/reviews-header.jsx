bb37f3f51680f9103cb5745b3fd2d48a947e97ab
blvkct <blvkct@gmail.com>
2025-04-17 10:30:47 +0530

    added the reviews page headings

--- src/components/reviews/reviews-header.jsx
+++ src/components/reviews/reviews-header.jsx
@@ -3,18 +3,11 @@ import "../../css/reviews/reviews-header.css";
 
 function ReviewsHeader() {
   return (
-    <div className="reviews-header-container">
-      <h1 className="reviews-heading">Reviews Heading</h1>
-      <h2 className="reviews-slogan">Reviews slogan</h2>
-      <p className="reviews-content">Reviews content</p>
-      
-      <div className="review-platforms">
-        <div className="trustpilot-logo">
-          <img src="/Images/review sites/trustpilot.png" alt="Trustpilot" />
-        </div>
-        <div className="google-logo">
-          <img src="/Images/review sites/google.png" alt="Google Reviews" />
-        </div>
+    <div className="container-fluid row reviews-header-container">
+      <div className="reviews-header-sec col-8">
+        <h1 className="reviews-heading">Student & Parent Reviews</h1>
+        <p className="reviews-content">RDiscover how Strive Academics has helped students excel across K-12 subjects, UCAT preparation, and university admissions. From boosting confidence in school subjects to securing top medical and dental school offers, our expert tutors are making a difference. Read their stories below!
+        </p>
       </div>
     </div>
   );
